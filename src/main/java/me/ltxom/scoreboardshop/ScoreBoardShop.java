5de6ac51d94f93d42ffed3fba92754bfe90d28ea
LTXOM <ltxom@uw.edu>
2020-04-25 11:38:33 -0700

    Fix ArrayIndexOutOfBoundsException

--- src/main/java/me/ltxom/scoreboardshop/ScoreBoardShop.java
+++ src/main/java/me/ltxom/scoreboardshop/ScoreBoardShop.java
@@ -68,119 +68,123 @@ public class ScoreBoardShop extends JavaPlugin {
 				promptService.getHelp(sender);
 				return true;
 			}
-			if (args[0].equals("link")) {
-				if (args[1].equals("list")) {
-					if (sender.hasPermission("me.ltxom.sbs.link")) {
-						// list all links
-						scoreboardLinkService.listAllLinks(sender);
+			try {
+				if (args[0].equals("link")) {
+					if (args[1].equals("list")) {
+						if (sender.hasPermission("me.ltxom.sbs.link")) {
+							// list all links
+							scoreboardLinkService.listAllLinks(sender);
+						} else {
+							// prompt no permission
+							promptService.noPermission(sender);
+						}
+
+					} else if (sender.hasPermission("me.ltxom.sbs.link")) {
+						try {
+							String scoreboardVar = args[1];
+							String displayName = args[2];
+							if (scoreboardVar != null && displayName != null && !scoreboardVar.isEmpty() && !displayName.isEmpty()) {
+								ResultCode resultCode = scoreboardLinkService.linkScoreboard(scoreboardVar,
+										displayName);
+								switch (resultCode) {
+									case CODE_OK:
+										promptService.linkedScoreboard(sender);
+										break;
+									case SCOREBOARD_DNE:
+										promptService.scoreboardDNE(sender);
+										break;
+									case IO_EXCEPTION:
+										promptService.exception(sender);
+										break;
+								}
+							} else {
+								// command doesn't have valid args
+								promptService.commandInvalid(sender);
+							}
+						} catch (IllegalArgumentException | IndexOutOfBoundsException e) {
+							// prompt not valid command
+							promptService.commandInvalid(sender);
+						}
 					} else {
 						// prompt no permission
 						promptService.noPermission(sender);
 					}
-
-				} else if (sender.hasPermission("me.ltxom.sbs.link")) {
-					try {
-						String scoreboardVar = args[1];
-						String displayName = args[2];
-						if (scoreboardVar != null && displayName != null && !scoreboardVar.isEmpty() && !displayName.isEmpty()) {
-							ResultCode resultCode = scoreboardLinkService.linkScoreboard(scoreboardVar,
-									displayName);
-							switch (resultCode) {
-								case CODE_OK:
-									promptService.linkedScoreboard(sender);
-									break;
-								case SCOREBOARD_DNE:
-									promptService.scoreboardDNE(sender);
-									break;
-								case IO_EXCEPTION:
-									promptService.exception(sender);
-									break;
-							}
+				} else if (args[0].equals("unlink")) {
+					if (sender.hasPermission("me.ltxom.sbs.unlink")) {
+						// unlink
+					} else {
+						// prompt no permission
+					}
+				} else if (args[0].equals("category")) {
+					if (args[1].equals("create")) {
+						if (sender.hasPermission("me.ltxom.sbs.category.create")) {
+							// How do you want to handle unexpected inputs?
+							// Create a category
+							String categoryName = args[2];
+							String displayName = args[3];
+							String displayItem = args[4];
 						} else {
-							// command doesn't have valid args
-							promptService.commandInvalid(sender);
+							// prompt no permission
 						}
-					} catch (IllegalArgumentException | IndexOutOfBoundsException e) {
-						// prompt not valid command
-						promptService.commandInvalid(sender);
+					} else if (args[1].equals("remove")) {
+						if (sender.hasPermission("me.ltxom.sbs.category.remove")) {
+							// Remove a category
+							String categoryName = args[2];
+						} else {
+							// prompt no permission
+						}
+					} else if (args[1].equals("list")) {
+						if (sender.hasPermission("me.ltxom.sbs.category.remove")) {
+							// List all categories
+						} else {
+							// prompt no permission
+						}
+					}
+				} else if (args[0].equals("item")) {
+					if (args[1].equals("create")) {
+						if (sender.hasPermission("me.ltxom.sbs.item.create")) {
+							// Create an item
+							String categoryName = args[2];
+							String scoreboardVarName = args[3];
+							Integer price = Integer.parseInt(args[4]);
+							String itemType = args[5];
+							String itemDesc = args[6];
+						} else {
+							// prompt no permission
+						}
+					} else if (args[1].equals("list")) {
+						if (sender.hasPermission("me.ltxom.sbs.item.list")) {
+							// List an item
+							String categoryName = args[2];
+						} else {
+							// prompt no permission
+						}
+					} else if (args[1].equals("remove")) {
+						if (sender.hasPermission("me.ltxom.sbs.item.remove")) {
+							// Remove an item
+							String categoryName = args[2];
+							Integer itemID = Integer.parseInt(args[3]);
+						} else {
+							// prompt no permission
+						}
+					}
+				} else if (args[0].equals("reload")) {
+					if (sender.hasPermission("me.ltxom.sbs.reload")) {
+						// Reload
+					} else {
+						// prompt no permission
+					}
+				} else if (args[0].equals("shop")) {
+					if (sender.hasPermission("me.ltxom.sbs.shop")) {
+						// Show the shop
+					} else {
+						// prompt no permission
 					}
 				} else {
-					// prompt no permission
-					promptService.noPermission(sender);
+					// Incorrect command || No Permission
 				}
-			} else if (args[0].equals("unlink")) {
-				if (sender.hasPermission("me.ltxom.sbs.unlink")) {
-					// unlink
-				} else {
-					// prompt no permission
-				}
-			} else if (args[0].equals("category")) {
-				if (args[1].equals("create")) {
-					if (sender.hasPermission("me.ltxom.sbs.category.create")) {
-						// How do you want to handle unexpected inputs?
-						// Create a category
-						String categoryName = args[2];
-						String displayName = args[3];
-						String displayItem = args[4];
-					} else {
-						// prompt no permission
-					}
-				} else if (args[1].equals("remove")) {
-					if (sender.hasPermission("me.ltxom.sbs.category.remove")) {
-						// Remove a category
-						String categoryName = args[2];
-					} else {
-						// prompt no permission
-					}
-				} else if (args[1].equals("list")) {
-					if (sender.hasPermission("me.ltxom.sbs.category.remove")) {
-						// List all categories
-					} else {
-						// prompt no permission
-					}
-				}
-			} else if (args[0].equals("item")) {
-				if (args[1].equals("create")) {
-					if (sender.hasPermission("me.ltxom.sbs.item.create")) {
-						// Create an item
-						String categoryName = args[2];
-						String scoreboardVarName = args[3];
-						Integer price = Integer.parseInt(args[4]);
-						String itemType = args[5];
-						String itemDesc = args[6];
-					} else {
-						// prompt no permission
-					}
-				} else if (args[1].equals("list")) {
-					if (sender.hasPermission("me.ltxom.sbs.item.list")) {
-						// List an item
-						String categoryName = args[2];
-					} else {
-						// prompt no permission
-					}
-				} else if (args[1].equals("remove")) {
-					if (sender.hasPermission("me.ltxom.sbs.item.remove")) {
-						// Remove an item
-						String categoryName = args[2];
-						Integer itemID = Integer.parseInt(args[3]);
-					} else {
-						// prompt no permission
-					}
-				}
-			} else if (args[0].equals("reload")) {
-				if (sender.hasPermission("me.ltxom.sbs.reload")) {
-					// Reload
-				} else {
-					// prompt no permission
-				}
-			} else if (args[0].equals("shop")) {
-				if (sender.hasPermission("me.ltxom.sbs.shop")) {
-					// Show the shop
-				} else {
-					// prompt no permission
-				}
-			} else {
-				// Incorrect command || No Permission
+			} catch (ArrayIndexOutOfBoundsException e) {
+				promptService.commandInvalid(sender);
 			}
 		}
 
